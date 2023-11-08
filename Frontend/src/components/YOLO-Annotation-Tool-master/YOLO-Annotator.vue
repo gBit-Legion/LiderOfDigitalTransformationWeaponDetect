e9cbfa06c2d505fd4c4b3763e2859a0945d98e79
WebApp777 <killsystem78@gmail.com>
2023-11-08 23:50:19 +0300

    front (Annotate)

--- Frontend/src/components/YOLO-Annotation-Tool-master/YOLO-Annotator.vue
+++ Frontend/src/components/YOLO-Annotation-Tool-master/YOLO-Annotator.vue
@@ -1,6 +1,6 @@
 <template>
     <body class="body">
-        <iframe src="http://127.0.0.1:5500/Annotate.html" width="1200vh" height="800px" frameborder="0"></iframe>
+        <iframe src="http://26.234.143.237:8000/static/Annotate.html" width="1200vh" height="800px" frameborder="0"></iframe>
     </body>
 </template>
 <script>
