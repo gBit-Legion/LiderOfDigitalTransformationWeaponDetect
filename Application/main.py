12c94ff09a5275fa91e482319660882356490b29
johnseeena <82346607+johnseeena@users.noreply.github.com>
2023-11-11 16:08:50 +0300

    Update main.py

--- Application/main.py
+++ Application/main.py
@@ -15,6 +15,8 @@ from starlette.responses import StreamingResponse
 from Services.CameraVideoCapture import RTSPCamera
 from Services.CodesForInteraction import *
 from Database.querry_to_database import *
+from Services.yolo_connet import class_colors
+from Services.yolo_connet import *
 
 app = FastAPI()
 
@@ -29,36 +31,115 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-stop_stream = False
+
+# stop_stream = False
+#
+#
+# async def video_stream(websocket: WebSocket, rtsp_url: str):
+#     await websocket.accept()
+#
+#     video_capture = cv2.VideoCapture(rtsp_url)
+#
+#     while True:
+#         ret, frame = video_capture.read()
+#
+#         if not ret:
+#             break
+#
+#         # Преобразуем кадр в формат JPEG
+#         _, buffer = cv2.imencode('.jpg', frame)
+#
+#         # Преобразуем кадр в строку base64
+#         frame_data = buffer.tobytes()
+#
+#         # Отправляем кадр на фронтенд
+#         await websocket.send(frame_data)
+#
+#     video_capture.release()
+#
+#
+# @app.websocket("//serve/{camera_id}")
+# async def video_feed(websocket: WebSocket, camera_id: int):
+#     rtsp_url = ["rtsp://admin:A1234567@188.170.176.190:8025 /Streaming/Channels/101?transportmode=unicast&profile=Profile_1"]
+#     await video_stream(websocket, rtsp_url)
+class RTSPCamera:
+    def __init__(self, rtsp_links, save_labels_folder, save_frames_folder):
+        self.rtsp_links = rtsp_links
+        for i in rtsp_links:
+            self.rtsp_links.append(i)
+        self.save_labels_folder = save_labels_folder
+        self.save_frames_folder = save_frames_folder
+
+    def process_video(self):
+        capture = cv2.VideoCapture(self.rtsp_links[0])
+        capture.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+        capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+        capture.set(cv2.CAP_PROP_POS_MSEC, 100)
+
+        if not capture.isOpened():
+            print(self.rtsp_links[0])
+            raise Exception(f'Failed to open RTSP link: {self.rtsp_links[0]}')
+
+        frame_count = 0
+
+        class_labels = ['knife', 'pistol', 'gun', 'riffle']
+
+        while True:
+            ret, frame = capture.read()
+
+            if ret:
+                result = model(frame, conf=0.3)
+
+                for result_item in result[0]:
+                    boxes = result_item[:, :4].cpu().numpy()
+                    classes = result_item[:, 5].cpu().numpy()
+                    confidences = result_item[:, 4].cpu().numpy()
+
+                    for box, cls, conf in zip(boxes, classes, confidences):
+                        x1, y1, x2, y2 = box.astype(int)
+                        cls = int(cls)
+                        conf = round(float(conf), 2)
+
+                        label = class_labels[cls] + str(conf)
+                        box_color = class_colors.get(cls, (255, 255, 255))
+
+                        (label_width, label_height), _ = cv2.getTextSize(label, class_font, class_font_scale, 1)
+                        text_position = (x1, y1 - 3 - label_height)
+
+                        cv2.rectangle(frame, (x1, y1), (x2, y2), box_color, 2)
+                        cv2.putText(frame, label, text_position, class_font, class_font_scale, box_color, 2)
+
+                        if frame_count % 30 == 0:
+                            save_frame_path = os.path.join(self.save_frames_folder, f'frame_{frame_count}.jpg')
+                            cv2.imwrite(save_frame_path, frame)
+
+                            frame_height, frame_width, _ = frame.shape
+                            x_center = (x1 + x2) / 2 / frame_width
+                            y_center = (y1 + y2) / 2 / frame_height
+                            box_width = (x2 - x1) / frame_width
+                            box_height = (y2 - y1) / frame_height
+
+                            txt_file_path = os.path.join(self.save_labels_folder, f'frame_{frame_count}.txt')
+                            with open(txt_file_path, 'w') as txt_file:
+                                txt_file.write(f'{cls} {x_center} {y_center} {box_width} {box_height}\n')
+
+                        frame_count += 1
+
+                    cv2.imshow("Cam", frame)
+
+                _, jpeg = cv2.imencode('.jpg', frame)
+                frame_bytes = jpeg.tobytes()
+                yield (b'--frame\r\n'
+                       b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n\r\n')
+
+            return StreamingResponse(self.process_video(), media_type="multipart/x-mixed-replace; boundary=frame")
 
 
-async def video_stream(websocket: WebSocket, rtsp_url: str):
-    await websocket.accept()
-
-    video_capture = cv2.VideoCapture(rtsp_url)
-
-    while True:
-        ret, frame = video_capture.read()
-
-        if not ret:
-            break
-
-        # Преобразуем кадр в формат JPEG
-        _, buffer = cv2.imencode('.jpg', frame)
-
-        # Преобразуем кадр в строку base64
-        frame_data = buffer.tobytes()
-
-        # Отправляем кадр на фронтенд
-        await websocket.send(frame_data)
-
-    video_capture.release()
-
-
-@app.websocket("//serve/{camera_id}")
-async def video_feed(websocket: WebSocket, camera_id: int):
-    rtsp_url = ["rtsp://admin:A1234567@188.170.176.190:8025 /Streaming/Channels/101?transportmode=unicast&profile=Profile_1"]
-    await video_stream(websocket, rtsp_url)
+@app.post("/serve/{camera_id}")
+async def get_video_stream(camera_id: int):
+    url = ["rtsp://admin:A1234567@188.170.176.190:8025 /Streaming/Channels/101?transportmode=unicast&profile=Profile_1"]
+    camera = RTSPCamera("url", "./labels", "./images")
+    return camera.process_video()
 
 
 @app.post("/getlist")
@@ -206,7 +287,7 @@ def get_static_image(filename: str, video_name: str):
         print("Папка уже существует.")
 
     # Определите путь к файлу на сервере FastAPI
-    file_path = os.path.join(os.path.join(video_name), filename)
+    file_path = os.path.join("./image", os.path.join(video_name, filename))
 
     return FileResponse(file_path)
 
